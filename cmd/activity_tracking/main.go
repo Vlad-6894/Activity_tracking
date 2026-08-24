e4cb86a42a60b5405194d47dfb898dc6242d9f45
GlebP <state-florida@mail.ru>
2026-08-24 22:07:49 +0400

    fix(logger): restore zap logger and LoggerConfig in main

--- cmd/activity_tracking/main.go
+++ cmd/activity_tracking/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"log/slog"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,10 +10,17 @@ import (
 	core_config "github.com/Vlad-6894/Activity_tracking/internal/core/config"
 	core_db "github.com/Vlad-6894/Activity_tracking/internal/core/db"
 	core_logger "github.com/Vlad-6894/Activity_tracking/internal/core/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
-	logger := core_logger.Init()
+	logger, err := core_logger.NewLogger(core_logger.NewLoggerConfigMust())
+	if err != nil {
+		fmt.Println("init logger error:", err)
+		os.Exit(1)
+	}
+	defer logger.Close()
+
 	logger.Info("Логер успешно инициализирован")
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
@@ -23,7 +30,7 @@ func main() {
 
 	pool, err := core_db.New(ctx, cfg)
 	if err != nil {
-		logger.Error("Ошибка подключения к PostgreSQL пулу", slog.String("error", err.Error()))
+		logger.Error("Ошибка подключения к PostgreSQL пулу", zap.Error(err))
 		os.Exit(1)
 	}
 	defer pool.Close()
--- internal/core/logger/logger.go
+++ internal/core/logger/logger.go
@@ -1,18 +1,92 @@
 package core_logger
 
 import (
-	"log/slog"
+	"context"
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func Init() *slog.Logger {
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+type loggerContextKey struct{}
+
+var (
+	key = loggerContextKey{}
+)
+
+type Logger struct {
+	*zap.Logger
+	file *os.File
+}
+
+func ToContext(ctx context.Context, log *Logger) context.Context {
+	return context.WithValue(
+		ctx,
+		key,
+		log,
+	)
+}
+
+func FromContext(ctx context.Context) *Logger {
+	log, ok := ctx.Value(key).(*Logger)
+	if !ok {
+		panic("No logger in the context!")
 	}
 
-	handler := slog.NewJSONHandler(os.Stdout, opts)
-	logger := slog.New(handler)
-	slog.SetDefault(logger)
+	return log
+}
 
-	return logger
+func NewLogger(config LoggerConfig) (*Logger, error) {
+	zapLvl := zap.NewAtomicLevel()
+	if err := zapLvl.UnmarshalText([]byte(config.Level)); err != nil {
+		return nil, fmt.Errorf("Unmarshal log level error: %w", err)
+	}
+
+	if err := os.MkdirAll(config.Folder, 0755); err != nil {
+		return nil, fmt.Errorf("Mkdirerror: %w", err)
+	}
+
+	timestamp := time.Now().UTC().Format("2006-01-02T15-04-05.000000")
+	logFilePath := filepath.Join(
+		config.Folder,
+		fmt.Sprintf("%s.log", timestamp),
+	)
+
+	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("log file open error: %w", err)
+	}
+
+	zapConfig := zap.NewDevelopmentEncoderConfig()
+	zapConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02T15:04:05.000000")
+
+	zapEncoder := zapcore.NewConsoleEncoder(zapConfig)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(zapEncoder, zapcore.AddSync(os.Stdout), zapLvl),
+		zapcore.NewCore(zapEncoder, zapcore.AddSync(logFile), zapLvl),
+	)
+
+	zapLogger := zap.New(core, zap.AddCaller())
+
+	return &Logger{
+		Logger: zapLogger,
+		file:   logFile,
+	}, nil
+}
+
+func (l *Logger) Close() {
+	if err := l.file.Close(); err != nil {
+		fmt.Println("Failed to close logger: ", err)
+	}
+}
+
+func (l *Logger) With(fields ...zap.Field) *Logger {
+	return &Logger{
+		Logger: l.Logger.With(fields...),
+		file:   l.file,
+	}
 }
