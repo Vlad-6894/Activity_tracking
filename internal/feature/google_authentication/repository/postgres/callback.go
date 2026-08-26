89904fb6d6040ea9e61ce6c3e23fb303ee57e22a
Vlad-6894 <vladpurchel40@gmail.com>
2026-08-26 23:08:10 +0300

    fix

--- internal/feature/google_authentication/repository/postgres/callback.go
+++ internal/feature/google_authentication/repository/postgres/callback.go
@@ -2,6 +2,7 @@ package authRepositoryPostgres
 
 import (
 	"context"
+	"fmt"
 
 	"golang.org/x/oauth2"
 )
@@ -11,7 +12,18 @@ func (r AuthRepositoryPostgres) SaveToken(
 	userID string,
 	token *oauth2.Token,
 ) error {
+	ctxWithTime, cancel := context.WithTimeout(ctx, r.pool.GetTimeot())
+	defer cancel()
+
 	query := `
-		
+		UPDATE app.users 
+		SET google_refresh_token = $1
+		WHERE id = $2;
 	`
+
+	if _, err := r.pool.Exec(ctxWithTime, query, token.RefreshToken, userID); err != nil {
+		return fmt.Errorf("fail to save refresh token: %w", err)
+	}
+
+	return nil
 }
